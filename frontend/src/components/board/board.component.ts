1f1b8c0cb6251228ec1b9213078cffa40e74d104
Muhammad Mustafa <momustafa683@gmail.com>
2024-08-11 07:35:17 +0300

    refactor(board): refactor the board to use 2D array instead of 1D

--- frontend/src/components/board/board.component.ts
+++ frontend/src/components/board/board.component.ts
@@ -11,20 +11,23 @@ import { INITIAL_PIECES_SETUP } from "../boardcell/piece.model";
   styleUrl: './board.component.css'
 })
 export class BoardComponent {
-  cells: BoardCell[] = [];
+  board: BoardCell[][] = [];
 
   constructor() {
     this.initBoard();
   }
 
   private initBoard(): void {
-    this.cells = Array.from({ length: 64 }, (_, i) => {
-      const row = Math.floor(i / 8); // Determine the row number (0 to 7)
-      const col = i % 8; // Determine the column number (0 to 7)
-      const isLight = (row + col) % 2 === 0; // Determine color based on row and column
-      const piece = INITIAL_PIECES_SETUP[row]?.[col]; // Determine the initial piece
+    const BOARD_SIZE = 8;
 
-      return new BoardCell(isLight, piece);
-    });
+    this.board = Array.from({ length: BOARD_SIZE }, (_, row) =>
+      Array.from({ length: BOARD_SIZE }, (_, col) => {
+        const isLight = (row + col) % 2 === 0; // Determine color based on row and column
+        const piece = INITIAL_PIECES_SETUP[row]?.[col]; // Determine the initial piece
+
+        return new BoardCell(isLight, piece, { row, col });
+      })
+    );
   }
+
 }
